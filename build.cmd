459423a80fec705c9cf8648808598912a551d706
aaron-hardin <teslasprotege@gmail.com>
2014-07-15 21:05:44 -0500

    removing old build.cmd
    gets packaged by server

--- build.cmd
+++
@@ -1,6 +0,0 @@
-copy src\MFiles.EntityFramework.PowerShell\bin\Debug\MFiles.EntityFramework.PowerShell.dll Packaging\tools\MFiles.EntityFramework.PowerShell.dll
-copy src\MFiles.EntityFramework.PowerShell.Helper\bin\Debug\MFiles.EntityFramework.PowerShell.Helper.dll Packaging\tools\MFiles.EntityFramework.PowerShell.Helper.dll
-copy src\MFiles.EntityFramework\bin\Debug\MFiles.EntityFramework.dll Packaging\lib\net45\MFiles.EntityFramework.dll
-
-.\Packaging\redist\NuGet.exe pack Packaging\MFilesEntityFramework.nuspec
-move *.nupkg Release\
