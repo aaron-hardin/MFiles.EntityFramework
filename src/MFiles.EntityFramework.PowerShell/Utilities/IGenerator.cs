f2148ecc1f5ca57cd8cbd7a9e1a080334d5e96ce
aaron-hardin <teslasprotege@gmail.com>
2014-07-21 23:06:10 -0500

    trying to standardize generators

---
+++ src/MFiles.EntityFramework.PowerShell/Utilities/IGenerator.cs
@@ -0,0 +1,19 @@
+﻿using EnvDTE;
+
+namespace MFiles.EntityFramework.PowerShell.Utilities
+{
+	internal interface IGenerator
+	{
+		Project Project { get; set; }
+
+		bool Exists { get; }
+
+		string FilePath { get; }
+
+		string GenerateCode(bool partial = false);
+
+		void Update();
+
+		void CreateNew();
+	}
+}
